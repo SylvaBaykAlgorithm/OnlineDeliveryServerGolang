5a011b47f2552880044fb039d2fa760a8007f521
Kenneth C Ashwood jr <ashwood.kenneth@gmail.com>
2022-09-28 13:08:25 -0400

    correction to server

---
+++ server.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/SylvaBaykAlgorithm/OnlineDeliveryServerGolang/src/github.com/SylvaBaykAlgorithm/OnlineDeliveryServerGolang/users"
+)
+
+func main() {
+	fmt.Println(users.Checkout())
+}
